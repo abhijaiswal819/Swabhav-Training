1789666d0db705ee8f747cb53b6c90e23a9d0067
Abhishek Jaiswal <55281568+abhijaiswal819@users.noreply.github.com>
2023-01-22 16:27:50 +0530

    To check armstrong number

---
+++ Core Java/Armstrong.java
@@ -0,0 +1,32 @@
+package com.aurionpro.model;
+
+import java.util.Scanner;
+
+public class Armstrong {
+
+	public static void main(String[] args) {
+		System.out.println("Please enter the number: ");
+		Scanner sc=new Scanner(System.in);
+		int num=sc.nextInt();
+		int cnum = num;
+		
+		int count=0;
+	    while(cnum>0) {
+	    	cnum=cnum/10;
+	    	count++;
+	    }
+	    
+	    cnum=num;
+	    int sum=0;
+	    while(cnum != 0) {
+	      int numDigit = cnum % 10;
+	      sum = (int) (sum + Math.pow(numDigit,count));
+	      cnum = cnum/ 10;
+	    }
+	    if(sum == num)
+            System.out.println(num + " is an Armstrong Number.");
+        else
+            System.out.println(num + " is not an Armstrong Number.");
+	}
+
+}
