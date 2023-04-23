d6baf2d06a5a7bea7f78ddaa8a01ee88bc95b581
Abhishek Jaiswal <abhij347.aj@gmail.com>
2023-04-23 15:12:53 +0530

    Till 23-4-23

--- Spring Core/15-bank-spring-app/src/main/java/com/aurionpro/entity/Transaction.java
+++ Spring Core/15-bank-spring-app/src/main/java/com/aurionpro/entity/Transaction.java
@@ -22,8 +22,10 @@ public class Transaction {
 	@Column(name = "transaction_no")
 	private Long transactionNo;
 
-	private String transactionType;
-	private String transactionDetail;
+	private Long sender;
+	private Long receiver;
+	//private String transactionType;
+	//private String transactionDetail;
 	private String transactionDate;
 	private double amount;
 	private double balance;
@@ -37,12 +39,12 @@ public class Transaction {
 		super();
 	}
 
-	public Transaction(Long transactionNo, String transactionType, String transactionDetail, String transactionDate,
-			double amount, double balance, Accounts accounts) {
+	public Transaction(Long transactionNo, Long sender, Long receiver, String transactionDate, double amount,
+			double balance, Accounts accounts) {
 		super();
 		this.transactionNo = transactionNo;
-		this.transactionType = transactionType;
-		this.transactionDetail = transactionDetail;
+		this.sender = sender;
+		this.receiver = receiver;
 		this.transactionDate = transactionDate;
 		this.amount = amount;
 		this.balance = balance;
@@ -57,20 +59,20 @@ public class Transaction {
 		this.transactionNo = transactionNo;
 	}
 
-	public String getTransactionType() {
-		return transactionType;
+	public Long getSender() {
+		return sender;
 	}
 
-	public void setTransactionType(String transactionType) {
-		this.transactionType = transactionType;
+	public void setSender(Long sender) {
+		this.sender = sender;
 	}
 
-	public String getTransactionDetail() {
-		return transactionDetail;
+	public Long getReceiver() {
+		return receiver;
 	}
 
-	public void setTransactionDetail(String transactionDetail) {
-		this.transactionDetail = transactionDetail;
+	public void setReceiver(Long receiver) {
+		this.receiver = receiver;
 	}
 
 	public String getTransactionDate() {
@@ -105,11 +107,88 @@ public class Transaction {
 		this.accounts = accounts;
 	}
 
-	@Override
-	public String toString() {
-		return "Transaction [transactionNo=" + transactionNo + ", transactionType=" + transactionType
-				+ ", transactionDetail=" + transactionDetail + ", transactionDate=" + transactionDate + ", amount="
-				+ amount + ", balance=" + balance + ", accounts=" + accounts + "]";
-	}
+//	@Override
+//	public String toString() {
+//		return "Transaction [transactionNo=" + transactionNo + ", sender=" + sender + ", receiver=" + receiver
+//				+ ", transactionDate=" + transactionDate + ", amount=" + amount + ", balance=" + balance + ", accounts="
+//				+ accounts + "]";
+//	}
+	
+	
+
+//	public Transaction(Long transactionNo, String transactionType, String transactionDetail, String transactionDate,
+//			double amount, double balance, Accounts accounts) {
+//		super();
+//		this.transactionNo = transactionNo;
+//		this.transactionType = transactionType;
+//		this.transactionDetail = transactionDetail;
+//		this.transactionDate = transactionDate;
+//		this.amount = amount;
+//		this.balance = balance;
+//		this.accounts = accounts;
+//	}
+//
+//	public Long getTransactionNo() {
+//		return transactionNo;
+//	}
+//
+//	public void setTransactionNo(Long transactionNo) {
+//		this.transactionNo = transactionNo;
+//	}
+//
+//	public String getTransactionType() {
+//		return transactionType;
+//	}
+//
+//	public void setTransactionType(String transactionType) {
+//		this.transactionType = transactionType;
+//	}
+//
+//	public String getTransactionDetail() {
+//		return transactionDetail;
+//	}
+//
+//	public void setTransactionDetail(String transactionDetail) {
+//		this.transactionDetail = transactionDetail;
+//	}
+//
+//	public String getTransactionDate() {
+//		return transactionDate;
+//	}
+//
+//	public void setTransactionDate(String transactionDate) {
+//		this.transactionDate = transactionDate;
+//	}
+//
+//	public double getAmount() {
+//		return amount;
+//	}
+//
+//	public void setAmount(double amount) {
+//		this.amount = amount;
+//	}
+//
+//	public double getBalance() {
+//		return balance;
+//	}
+//
+//	public void setBalance(double balance) {
+//		this.balance = balance;
+//	}
+//
+//	public Accounts getAccounts() {
+//		return accounts;
+//	}
+//
+//	public void setAccounts(Accounts accounts) {
+//		this.accounts = accounts;
+//	}
+
+//	@Override
+//	public String toString() {
+//		return "Transaction [transactionNo=" + transactionNo + ", transactionType=" + transactionType
+//				+ ", transactionDetail=" + transactionDetail + ", transactionDate=" + transactionDate + ", amount="
+//				+ amount + ", balance=" + balance + ", accounts=" + accounts + "]";
+//	}
 
 }
--- Spring Core/15-bank-spring-app/src/main/java/com/aurionpro/service/TransactionServiceImpl.java
+++ Spring Core/15-bank-spring-app/src/main/java/com/aurionpro/service/TransactionServiceImpl.java
@@ -1,13 +1,17 @@
 package com.aurionpro.service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.aurionpro.entity.Accounts;
 import com.aurionpro.entity.Customer;
 import com.aurionpro.entity.Transaction;
+import com.aurionpro.repository.AccountsRepository;
 import com.aurionpro.repository.CustomerRepository;
 import com.aurionpro.repository.TransactionRepository;
 
@@ -17,10 +21,98 @@ public class TransactionServiceImpl implements TransactionService {
 	@Autowired
 	private TransactionRepository transactionRepository;
 
+	@Autowired
+	private AccountsRepository accountRepository;
+
+	@Autowired
+	private CustomerRepository customerRepository;
+
 	@Override
 	public ResponseEntity<String> saveTransaction(Transaction transactionData) {
-		transactionRepository.save(transactionData);
-		return ResponseEntity.ok("Transaction data saved");
+
+		if (accountRepository.existsById(transactionData.getSender())) {
+			if (accountRepository.existsById(transactionData.getReceiver())) {
+				LocalDate transactionDate = LocalDate.now();
+				String today = transactionDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+				transactionData.setTransactionDate(today);
+
+				// All About Sender
+				Accounts senderAccount = accountRepository.findById(transactionData.getSender()).get();
+				senderAccount.setBalance(senderAccount.getBalance() - transactionData.getAmount());
+				transactionData.setBalance(senderAccount.getBalance());
+
+				//// Mapping transaction list to sender account
+				List<Transaction> transactionList = senderAccount.getTransactions();
+//										for(Transaction x: transactionList) {
+//											x.setBalance(account.getBalance());
+//											x.setCustomer(customer);
+//											//x=account;
+//										}
+				transactionList.add(transactionData);
+				senderAccount.setTransactions(transactionList);
+				accountRepository.save(senderAccount);
+
+				System.out.println();
+				transactionRepository.save(transactionData);
+
+				// All about Receiver
+				Accounts receiverAccount = accountRepository.findById(transactionData.getReceiver()).get();
+				receiverAccount.setBalance(receiverAccount.getBalance() + transactionData.getAmount());
+				transactionData.setBalance(receiverAccount.getBalance());
+
+				//// Mapping transaction list to receiver account
+				List<Transaction> transactionList1 = receiverAccount.getTransactions();
+//										for(Transaction x: transactionList) {
+//											x.setBalance(account.getBalance());
+//											x.setCustomer(customer);
+//											//x=account;
+//										}
+				transactionList1.add(transactionData);
+				receiverAccount.setTransactions(transactionList1);
+				
+				accountRepository.save(receiverAccount);
+				//transactionData.setTransactionNo();
+				Transaction newTransaction = new Transaction();
+				newTransaction.setAccounts(transactionData.getAccounts());
+				newTransaction.setAmount(transactionData.getAmount());
+				newTransaction.setBalance(transactionData.getBalance());
+				newTransaction.setReceiver(transactionData.getReceiver());
+				newTransaction.setSender(transactionData.getSender());
+				newTransaction.setTransactionDate(transactionData.getTransactionDate());
+				
+				System.out.println();
+				transactionRepository.save(newTransaction);
+
+				// senderAccount.setTransactions(transactionData);
+
+				Customer senderCustomer = customerRepository.findById(senderAccount.getCustomer().getCustomerId())
+						.get();
+				totalBalance(senderCustomer);
+				Customer receiverCustomer = customerRepository.findById(receiverAccount.getCustomer().getCustomerId())
+						.get();
+				totalBalance(receiverCustomer);
+
+				return ResponseEntity.ok("Transaction data saved");
+			}
+		}
+
+		return ResponseEntity.ok("Error");
+	}
+
+	private void totalBalance(Customer dbData) {
+
+		// Customer customer =
+		// customerRepository.findById(dbData.getCustomerId()).get();
+		List<Accounts> accounts = dbData.getAccounts();
+		double totalBalance = 0;
+		for (Accounts x : accounts) {
+			double bal = x.getBalance();
+			totalBalance += bal;
+		}
+		dbData.setTotalBalance(totalBalance);
+		dbData.setCustomerId(dbData.getCustomerId());
+		customerRepository.save(dbData);
+		System.out.println("Current Total Balance is " + totalBalance);
 	}
 
 	@Override
